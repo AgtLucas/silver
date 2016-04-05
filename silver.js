b6140e56ab937891af06957fa626a69412589c4a
Lucas <lucas@agtlucas.com>
2016-04-05 11:38:22 -0300

    Add codeGenerator function

--- silver.js
+++ silver.js
@@ -155,6 +155,7 @@ function traverser (ast, visitor) {
   traverseNode(ast, null)
 }
 
+// Transformer
 function transformer (ast) {
   var newAst = {
     type: 'Program',
@@ -195,4 +196,27 @@ function transformer (ast) {
   })
 
   return newAst
+}
+
+// Code Generator
+function codeGenerator (node) {
+  switch (node.type) {
+    case 'Program':
+      return node.body.map(codeGenerator)
+        .join('\n')
+    case 'ExpressionStatement':
+      return (
+        codeGenerator(node.expression)
+      )
+    case 'CallExpression':
+      return (
+        codeGenerator(node.callee) + '(' + node.arguments.map(codeGenerator).join(', ') + ')'
+      )
+    case 'Identifier':
+      return node.name
+    case 'NumberLiteral':
+      return node.value
+    default:
+      throw new TypeError(node.type)
+  }
 }
